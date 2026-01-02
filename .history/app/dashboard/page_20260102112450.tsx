3ffbb5727e0775b2b984fc336ba05dd5fba2de8d
IreneC16 <you@example.com>
2026-01-02 20:52:39 -0300

    chore: ignorar arquivos de histórico do editor

--- .history/app/dashboard/page_20260102112450.tsx
+++
@@ -1,9 +0,0 @@
-import { Navbar } from "@/components/Navbar";
-
-export default function DashboardPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl text-neon-blue">Dashboard</h1>
-    </div>
-  );
-}
