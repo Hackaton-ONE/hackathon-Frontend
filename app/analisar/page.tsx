96df89b71352839003ecb8e4a5aa79b09a340014
Vicente <vvenancio06@gmail.com>
2026-01-05 17:27:27 -0300

    feat: componente input e retorno do sentimento

--- app/analisar/page.tsx
+++ app/analisar/page.tsx
@@ -1,16 +1,28 @@
 import { Navbar } from "@/components/Navbar";
+import Input_sentimento from "./Componentes/Input_sentimento";
+import Sentimentos from "./Componentes/Sentimentos";
+import Image from "next/image";
 
 export default function AnalisarPage() {
   return (
-    <div className="min-h-screen bg-mood-dark"> {/* Adicionei bg-mood-dark para garantir o fundo */}
-      
-      {/* 1. A Navbar entra aqui, DENTRO do return */}
+    <div className="min-h-screen bg-mood-dark">
       <Navbar />
 
-      {/* 2. O conteúdo da página vem depois */}
-      <div className="flex flex-col items-center justify-center pt-32"> {/* pt-32 para não ficar atrás da navbar */}
-        <h1 className="text-2xl text-neon-blue font-bold">Analisar</h1>
-      </div>
+      <main className="flex flex-col justify-center gap-5 ">
+        <Input_sentimento />
+        
+        <div className="flex justify-center">
+          <Image
+            width={100}  
+            height={10}
+            src="/seta_analisar.png"
+            alt="Seta"
+            className=""
+          />
+        </div>
+
+        <Sentimentos />
+      </main>
     </div>
   );
 }
